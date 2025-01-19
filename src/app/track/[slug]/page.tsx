0657c97b7d2b7d8d1d09d1c138511738df92467e
ghostoflord <ghost2892003zzz@gmail.com>
2025-01-19 21:41:22 +0700

    update

---
+++ src/app/track/[slug]/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import WaveTrack from '@/components/track/wave.track';
+import { useSearchParams } from 'next/navigation'
+
+const DetailTrackPage = (props: any) => {
+    const { params } = props;
+    const searchParams = useSearchParams()
+    const search = searchParams.get('audio')
+    console.log(">>> check search: ", search)
+    return (
+        <div>DetailTrackPage
+            <div>
+                <WaveTrack />
+            </div>
+        </div>
+    )
+}
+export default DetailTrackPage;
