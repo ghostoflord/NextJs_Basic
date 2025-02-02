7dfc28c62dbfc851481f1c124ba379ca1e35e5be
ghostoflord <ghost2892003zzz@gmail.com>
2025-02-02 17:22:05 +0700

    179. Add Progress Bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,22 @@ import ThemeRegistry from '@/components/theme-registry/theme.registry';
 import NextAuthWrapper from '@/lib/next.auth.wrapper';
 import { TrackContextProvider } from '@/lib/track.wrapper';
 import { ToastProvider } from '@/utils/toast';
+import NProgressWrapper from '@/lib/nprogress.wrapper';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body>
                 <ThemeRegistry>
-                    <NextAuthWrapper>
-                        <ToastProvider>
-                            <TrackContextProvider>
-                                {children}
-                            </TrackContextProvider>
-                        </ToastProvider>
-                    </NextAuthWrapper>
+                    <NProgressWrapper>
+                        <NextAuthWrapper>
+                            <ToastProvider>
+                                <TrackContextProvider>
+                                    {children}
+                                </TrackContextProvider>
+                            </ToastProvider>
+                        </NextAuthWrapper>
+                    </NProgressWrapper>
                 </ThemeRegistry>
             </body>
         </html>
